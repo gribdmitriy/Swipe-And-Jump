77d36e0cfab044cdcae3c0e0310ebb7d603430d3
gribdmitriy <dmitrii.9rib@gmail.com>
2021-01-27 04:37:08 +0200

    [NOT FINAL COMMIT] continue button

--- Assets/Scripts/UI/GameUI/GameoverUI/GameOverContinueButton.cs
+++ Assets/Scripts/UI/GameUI/GameoverUI/GameOverContinueButton.cs
@@ -1,7 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameOverContinueButton : MonoBehaviour
 {
+    public float minimum = -1.0F;
+    public float maximum = 1.0F;
+
+    static float t = 0.0f;
+
+    public Color additiveStartColor;
+    public Color additiveEndColor;
+    public Image additiveImage;
+
+    private void Start()
+    {
+        additiveImage.color = new Color(255, 0, 0, 255);
+    }
+
+   
 
     public void Click()
     {
