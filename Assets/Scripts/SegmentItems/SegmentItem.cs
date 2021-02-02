986e6d3c10f8775bba00541ea6074a78c66e7492
gribdmitriy <dmitrii.9rib@gmail.com>
2021-02-02 20:08:03 +0200

    Revert "add script UpgradableItem"
    This reverts commit cecedb0db6ebcbed4664c3058f65ba6d4a91690a.

--- Assets/Scripts/SegmentItems/SegmentItem.cs
+++ Assets/Scripts/SegmentItems/SegmentItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class SegmentItem : UpgradableItem
+public abstract class SegmentItem : PoolObject
 {
     public Segment parentSegment;
 
